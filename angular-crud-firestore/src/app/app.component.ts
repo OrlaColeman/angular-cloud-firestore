44b588f353cdebd34d13f701a1be9efa04617d6d
Orla <orlaith.coleman@gmail.com>
2020-05-07 17:03:34 +0100

    fixed folders

--- angular-crud-firestore/src/app/app.component.ts
+++ angular-crud-firestore/src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CrudService } from './service/crud.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-crud-firestore';
+export class AppComponent implements OnInit{
+  title = 'firestore';
+
+  Details: any;
+  token = 'changedToken';
+  tokenID = '4zG7Ycgd2SVSdl3NDcLJ';
+  constructor(private crudService: CrudService){}
+
+  ngOnInit(){
+    this.crudService.readToken().subscribe(data => {
+
+      this.Details = data.map(e => {
+        return{
+          id: e.payload.doc.id,
+          LongAccessToken: e.payload.doc.data()['LongAccessToken'],
+        };
+      })
+      console.log(this.Details);
+    })
+  }
+
+  readToken(){
+    this.crudService.readToken().subscribe(data => {
+
+      this.Details = data.map(e => {
+        return{
+          id: e.payload.doc.id,
+          LongAccessToken: e.payload.doc.data()['LongAccessToken'],
+        };
+      })
+      console.log(this.Details);
+    })
+  }
+
+  createToken(){
+    let details = {};
+    details['LongAccessToken'] = this.token;
+    this.crudService.createToken(details);
+  }
+
+  updateToken(){
+    let details = {};
+    details['LongAccessToken'] = this.token;
+    this.crudService.updateToken(this.tokenID, details);
+  }
+  
+  deleteToken(){
+    
+    this.crudService.deleteToken(this.tokenID);
+  }
+
 }
--- angular-crud-firestore/src/app/app.module.ts
+++ angular-crud-firestore/src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { environment } from '../environments/environment';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
